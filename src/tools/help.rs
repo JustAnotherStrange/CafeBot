c1c7c13ee9ea786fa58b8db9bba38d12af9a3aa8
JustAnotherStrange <olinder1300@gmail.com>
2021-04-17 16:27:44 -0400

    am dumb

--- src/tools/help.rs
+++ src/tools/help.rs
@@ -13,7 +13,7 @@ async fn help(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
     match args.rest() {
         "" => {
             let response = MessageBuilder::new()
-                .push_bold_safe("Welcome to CafeBot v0.2.0!\n \n")
+                .push_bold_safe("Welcome to CafeBot v0.1.0!\n \n")
                 .push("Commands:\n")
                 .push("**^help [page]** - show help pages. Specify no page for the general help or use one of the following categories: admin\n")
                 .push("**^ping** - pong\n")
