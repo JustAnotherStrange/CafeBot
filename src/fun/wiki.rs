fe2df1d7c242ce5ac490ae5b2cdf36484f167196
jacob gw <jacoblevgw@gmail.com>
2021-04-18 14:45:06 -0400

    [feat] cleanup stuff
    BRUHHHHHHHH

--- src/fun/wiki.rs
+++ src/fun/wiki.rs
@@ -1,4 +1,3 @@
-use crate::rjq;
 use serde_json::Value;
 use serenity::{
     framework::standard::{macros::command, Args, CommandResult},
@@ -10,8 +9,8 @@ async fn wiki(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
     let choice = args.rest().to_string();
     let link = format!("https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch='{}'", choice);
     let json: Value = serde_json::from_str(&reqwest::get(&link).await?.text().await?).unwrap();
-    let mut ids: Vec<&String> = Vec::new();
-    let mut titles: Vec<String> = Vec::new();
+    let mut ids: Vec<&str> = Vec::new();
+    let mut titles: Vec<&str> = Vec::new();
     for (k, v) in match json["query"]["pages"].as_object() {
         Some(x) => x,
         None => {
@@ -24,7 +23,7 @@ async fn wiki(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
     {
         // v is the json value, k is the id of the page
         ids.push(k);
-        titles.push(rjq(v["title"].to_string()));
+        titles.push(v["title"].as_str().unwrap());
     }
     // print article names
     let mut descs = String::new();
@@ -35,7 +34,7 @@ async fn wiki(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
         let article_url = json_url["query"]["pages"][ids[i]]["fullurl"]
             .as_str()
             .unwrap();
-        let temp: String = format!("{}. **{}**: {}\n", i + 1, titles[i], article_url);
+        let temp = format!("{}. **{}**: {}\n", i + 1, titles[i], article_url);
         descs.push_str(&temp);
     }
     let title = format!("**Wikipedia results for *{}:***", choice);
